dbc1522975214f13485d5d77036d58e6e693f7fb
imlargo <jlargob@unal.edu.co>
2025-10-29 23:10:26 -0500

    _

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,9 +1,12 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { render } from 'svelte/server';
 
 export const POST: RequestHandler = async ({ request }) => {
     const data = await request.json();
-    const code  = data.code as string;
+    const code = data.code as string;
 
-    return await json({ compiled: code})
+    const rendered = await render(code)
+
+    return await json({ compiled: rendered })
 };
